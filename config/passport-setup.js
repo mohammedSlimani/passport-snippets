1335c927afcc98b361160172e9b19f1cc0ea04f7
Mohammed slimani <slimani.med.21@gmail.com>
2019-06-16 23:59:54 +0100

    enabled Google+ API

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require("passport-google-oauth20");
 passport.use(
     new GoogleStrategy({
         //options of the google-strat
+        clientID:'200253771844-jfev3qd2crmgnhir41f7kfr0f47ao5ld.apps.googleusercontent.com',
+        clientSecret:'GWcxMP_XmQd6u9-jlVXvuMW5'
     }),()=>{
         //passport cb
     })
